ec8be2600b6e221a8c521ace1e7532a0a14d8528
cme <cme_dream@163.com>
2019-06-24 18:46:40 +0800

    路由网关(zuul)(Finchley版本)

--- Readme.md
+++ Readme.md
@@ -2,3 +2,4 @@
 ### eureka-server   注册中心
 ### eureka-client   服务提供者
 ### service-ribbon  服务消费者
+### service-feign  服务消费者
---
+++ service-zuul/src/main/java/com/cme/cloud/servicezuul/ServicezuulApplication.java
@@ -0,0 +1,30 @@
+package com.cme.cloud.servicezuul;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+
+/**
+ * 在其入口applicaton类加上注解@EnableZuulProxy，开启zuul的功能
+ *
+ * @EnableDiscoveryClient 向注册注册服务
+ */
+
+@SpringBootApplication
+@EnableZuulProxy
+@EnableEurekaClient
+@EnableDiscoveryClient
+public class ServicezuulApplication {
+
+    private final static Logger log = LoggerFactory.getLogger(ServicezuulApplication.class);
+
+    public static void main(String[] args) {
+        SpringApplication.run(ServicezuulApplication.class, args);
+        log.info("路由器，过滤器启动成功");
+    }
+
+}
